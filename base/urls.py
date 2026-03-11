8626b222ae41778e75c9218a4bb163e4cbd6460e
Darpan242003 <bhamredarpan@gmail.com>
2026-03-12 00:41:59 +0530

    Added view, add and update Task views along with respective URLs and Templates

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('add task', views.add_task, name='add_task'),
+    path('update/<int:task_id>', views.update, name='update'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
 
 # Create your views here.
 def home(request):
 
-    return render(request, 'home.html')
+    all_tasks = Task.objects.all()
+
+    return render(request, 'home.html', {'tasks': all_tasks})
+
+def add_task(request):
+    if request.method == "POST":
+        title = request.POST["title"]
+        description = request.POST["description"]
+
+        task = Task.objects.create(
+            title = title,
+            description = description
+        )
+
+        return redirect('home')
+
+    return render(request, 'add.html')
+
+def update(request, task_id):
+    task = Task.objects.get(id = task_id)
+    
+    if request.method == "POST":
+        title = request.POST.get('title', default="Title")
+        description = request.POST.get('description', default="Description")
+
+        task.title = title
+        task.description = description
+        task.save()
+
+        return redirect('home')
+
+    return render(request, 'update.html', {'task':task})
+
+def mark_complete(request):
+
+    return render(request, 'complete.html')
